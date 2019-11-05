1e397ad94a90416e35f3d7e1038b7753aed4e197
ouyifan <ouyifan@DESKTOP-L32AQF2>
2019-11-05 15:34:59 +0800

    1

---
+++ ssm/src/main/java/com/bjsxt/controller/UsersController.java
@@ -0,0 +1,20 @@
+package com.bjsxt.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bjsxt.service.UsersService;
+
+@Controller
+public class UsersController {
+	@Resource
+	private UsersService usersServiceImpl;
+	@RequestMapping("show")
+	public String show(Model model){
+		model.addAttribute("list", usersServiceImpl.show());
+		return "index.jsp";
+	}
+}
---
+++ ssm/src/main/java/com/bjsxt/service/impl/UsersServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bjsxt.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.bjsxt.mapper.UsersMapper;
+import com.bjsxt.pojo.Users;
+import com.bjsxt.service.UsersService;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+	@Resource
+	private UsersMapper usersMapper;
+	@Override
+	public List<Users> show() {
+		return usersMapper.selAll();
+	}
+
+}
